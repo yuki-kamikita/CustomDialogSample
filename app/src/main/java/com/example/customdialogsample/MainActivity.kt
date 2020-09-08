70db9169cec96cc717ae027f6175e01f1988685c
shuhei-ff <s.kiyosawa@frontierfield.co.jp>
2020-09-08 18:57:27 +0900

    checkboxで選択した値をMainActivityへ返す

--- app/src/main/java/com/example/customdialogsample/MainActivity.kt
+++ app/src/main/java/com/example/customdialogsample/MainActivity.kt
@@ -2,23 +2,40 @@ package com.example.customdialogsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+        NoticeDialogFragment.NoticeDialogListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            confirmFireMissiles()
+            showNoticeDialog()
         }
 
     }
 
-    fun confirmFireMissiles() {
-        val newFragment = FireMissilesDialogFragment()
-        newFragment.show(supportFragmentManager, "missiles")
+    fun showNoticeDialog() {
+        // ダイアログフラグメントのインスタンスを作成して表示します
+        val dialog = NoticeDialogFragment()
+        dialog.show(supportFragmentManager, "NoticeDialogFragment")
     }
+
+    // ダイアログフラグメントは、Fragment.onAttach（）コールバックを通じてこのアクティビティへの参照を受け取ります。
+    // このコールバックは、NoticeDialogFragment.NoticeDialogListenerインターフェースで定義された
+    // 次のメソッドを呼び出すために使用します。
+    override fun onDialogPositiveClick(dialog: DialogFragment, selectedItems: ArrayList<Int>) {
+        // ユーザーがダイアログの正のボタンに触れた
+        textView.text = selectedItems.toString()
+    }
+
+    override fun onDialogNegativeClick(dialog: DialogFragment) {
+        // ユーザーがダイアログの否定ボタンに触れた
+        textView.text = "canceled"
+    }
+
 }
 
 // 出典：https://developer.android.com/guide/topics/ui/dialogs?hl=ja
--- app/src/main/java/com/example/customdialogsample/NoticeDialogFragment.kt
+++ app/src/main/java/com/example/customdialogsample/NoticeDialogFragment.kt
@@ -2,11 +2,36 @@ package com.example.customdialogsample
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
-class FireMissilesDialogFragment : DialogFragment() {
+class NoticeDialogFragment : DialogFragment() {
+    // インターフェースのこのインスタンスを使用して、アクションイベントを配信します
+    internal lateinit var listener: NoticeDialogListener
+
+    /* このダイアログフラグメントのインスタンスを作成するアクティビティは、
+     * イベントコールバックを受信するために、このインターフェイスを実装する必要があります。
+     * ホストがクエリする必要がある場合に備えて、各メソッドはDialogFragmentを渡します。 */
+    interface NoticeDialogListener {
+        fun onDialogPositiveClick(dialog: DialogFragment ,selectedItems: ArrayList<Int>)
+        fun onDialogNegativeClick(dialog: DialogFragment)
+    }
+
+    // Fragment.onAttach（）メソッドをオーバーライドして、NoticeDialogListenerをインスタンス化します。
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        // ホストアクティビティがコールバックインターフェイスを実装していることを確認する
+        try {
+            // ホストにイベントを送信できるように、NoticeDialogListenerをインスタンス化します
+            listener = context as NoticeDialogListener
+        } catch (e: ClassCastException) {
+            // アクティビティはインターフェースを実装していません、例外をスローします
+            throw ClassCastException((context.toString() +
+                    " must implement NoticeDialogListener"))
+        }
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
@@ -33,10 +58,12 @@ class FireMissilesDialogFragment : DialogFragment() {
                     DialogInterface.OnClickListener { dialog, id ->
                         // ユーザーが[OK]をクリックしたので、selectedItemsの結果をどこかに保存するか、
                         // ダイアログを開いたコンポーネントに返します
+                        listener.onDialogPositiveClick(this, selectedItems)
                     }
                 )
                 .setNegativeButton("cancel",
                     DialogInterface.OnClickListener { dialog, id ->
+                        listener.onDialogNegativeClick(this)
                     }
                 )
 
